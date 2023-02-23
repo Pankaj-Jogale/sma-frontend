a5412206fcfba020f637a01c7047c54b465a4db6
Pankaj RJ <pankajjogale3@gmail.com>
2023-02-23 14:15:34 +0530

    navbar-design

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Social_Media</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <input type="text" placeholder="Search" />
+          <SearchOutlinedIcon />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsNoneOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/3228732/pexels-photo-3228732.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <span>User Name</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
