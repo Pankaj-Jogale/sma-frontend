9ac06988fa062f01fdec564c715cf03b1bc61f89
Pankaj RJ <pankajjogale3@gmail.com>
2023-03-03 13:45:34 +0530

    backend connected with backend db

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.scss";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+  console.log(err);
   return (
     <div className="register">
       <div className="card">
@@ -21,12 +44,32 @@ const Register = () => {
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-
-            <button>Register</button>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            {err}
+            <button onClick={handleClick}>Register</button>
           </form>
         </div>
       </div>
