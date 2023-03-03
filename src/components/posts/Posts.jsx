237f08925cc157c0f396c661aa3dacf67a6d4fcd
Pankaj RJ <pankajjogale3@gmail.com>
2023-03-04 01:21:30 +0530

    getting post from db

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,41 +1,22 @@
 import Post from "../post/Post";
 import "./posts.scss";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
 
 const Posts = () => {
-  //temp dummy data
-  const posts = [
-    {
-      id: 1,
-      name: "User name",
-      userId: 1,
-      profilePic:
-        "https://images.pexels.com/photos/3228740/pexels-photo-3228740.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Paragraphs are the building blocks of papers.",
-      img: "https://images.pexels.com/photos/3228736/pexels-photo-3228736.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-    {
-      id: 2,
-      name: "User name",
-      userId: 2,
-      profilePic:
-        "https://images.pexels.com/photos/3228740/pexels-photo-3228740.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Paragraphs are the building blocks of papers.Paragraphs are the building blocks of papers.",
-    },
-    {
-      id: 3,
-      name: "User name",
-      userId: 3,
-      profilePic:
-        "https://images.pexels.com/photos/3228740/pexels-photo-3228740.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Paragraphs are the building blocks of papers.",
-      img: "https://images.pexels.com/photos/3228736/pexels-photo-3228736.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-  ];
+  const { isLoading, error, data } = useQuery(["posts"], () =>
+    makeRequest.get("/posts").then((res) => {
+      return res.data;
+    })
+  );
+  console.log(data);
   return (
     <div className="posts">
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
+      {error
+        ? "something went wrong"
+        : isLoading
+        ? "loading"
+        : data.map((post) => <Post post={post} key={post.id} />)}
     </div>
   );
 };
