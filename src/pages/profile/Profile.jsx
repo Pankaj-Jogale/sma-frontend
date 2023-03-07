484cdb43be09b9bd51163dd270a25a48429fc8dc
Pankaj RJ <pankajjogale3@gmail.com>
2023-03-07 11:36:32 +0530

    fetched only particular users post on profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -108,7 +108,7 @@ const Profile = () => {
                 <MoreVertIcon />
               </div>
             </div>
-            <Posts />
+            <Posts userId={userId} />
           </div>
         </>
       )}
