d632cb9875af8d67917178af6ca049779555a1ea
Pankaj RJ <pankajjogale3@gmail.com>
2023-03-08 17:15:24 +0530

    logout btn added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,16 +7,24 @@ import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNone
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { Link } from "react-router-dom";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { AuthContext } from "../../context/authContext";
+import axios from "axios";
 
 //
 const Navbar = () => {
   const { toggle, darkMode } = useContext(DarkModeContext);
   const { currentUser } = useContext(AuthContext);
-
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    // const res = await axios.post("http://localhost:8800/api/auth/logout");
+    // console.log(res.status);
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
   return (
     <div className="navbar">
       <div className="left">
@@ -37,6 +45,12 @@ const Navbar = () => {
         </div>
       </div>
       <div className="right">
+        <button
+          onClick={handleLogout}
+          style={{ border: "none", cursor: "pointer" }}
+        >
+          <LogoutOutlinedIcon />
+        </button>
         <PersonOutlineOutlinedIcon />
         <EmailOutlinedIcon />
         <NotificationsNoneOutlinedIcon />
